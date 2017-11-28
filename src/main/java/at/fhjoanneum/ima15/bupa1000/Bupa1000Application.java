0ea803c4c4000a8131645614d3b313d59ad115e8
Sebastian Wanke <sebastian.wanke@gmx.at>
2017-11-28 15:34:10 +0100

    Intial Project commit

---
+++ src/main/java/at/fhjoanneum/ima15/bupa1000/Bupa1000Application.java
@@ -0,0 +1,12 @@
+package at.fhjoanneum.ima15.bupa1000;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bupa1000Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bupa1000Application.class, args);
+	}
+}
---
+++ src/test/java/at/fhjoanneum/ima15/bupa1000/Bupa1000ApplicationTests.java
@@ -0,0 +1,16 @@
+package at.fhjoanneum.ima15.bupa1000;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Bupa1000ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
