629f803d65e583bdf54239315372fbe430fb7294
kiv1995 <kstessel@gmx.at>
2017-12-02 14:18:12 +0100

    ein paar Attribute eingefügt

--- src/main/java/at/fhjoanneum/ima15/bupa1000/model/Source.java
+++ src/main/java/at/fhjoanneum/ima15/bupa1000/model/Source.java
@@ -1,4 +1,15 @@
 package at.fhjoanneum.ima15.bupa1000.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public class Source {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String name;
+
 }
